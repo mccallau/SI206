e006356af58be88534d4c99a9830489a9e150443
mccallau <mccallau@umich.edu>
2017-10-03 23:05:32 -0400

    20171003 1105pm

--- HW5/HW5.py
+++ HW5/HW5.py
@@ -2,8 +2,7 @@ import re
 file = open("regex_sum_31742.txt",'r')
 allnum = []
 for line in file:
-	if len(re.findall('[0-9]',line))>0:
-		num = re.findall('[0-9]+',line)
-		for n in num:
-			allnum.append(int(n))
+	num = re.findall('[0-9]+',line)
+	for n in num:
+		allnum.append(int(n))
 print(sum(allnum))
