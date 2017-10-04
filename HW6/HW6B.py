20e87a15ff14302d8489b8b98c519b2c8b25d2ad
mccallau <mccallau@umich.edu>
2017-10-04 16:05:54 -0400

    20171004 405pm

--- HW6/HW6B.py
+++ HW6/HW6B.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import urllib.request, urllib.error, urllib.parse
-url = input('Enter - ')
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html,'html.parser')
-tags = soup('span')
-numlist = []
-for strings in tags:
-	numlist.append(int(strings.string))
-print("Count: ",len(numlist))
-print("Sum: ",sum(numlist))
+url = (input('Enter URL - ')).strip()
+count = int(input('Enter Count - '))
+position = int(input('Enter Position - '))
+print("Retrieving URL: ",url)
+for x in range(count):
+	html = urllib.request.urlopen(url).read()
+	soup = BeautifulSoup(html,'html.parser')
+	tags = soup('a')
+	print("Retrieving URL: " ,tags[position-1].get("href",None))
+	url = tags[position-1].get("href",None)
